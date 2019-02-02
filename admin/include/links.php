da3be791c301280a6c2f252a79dc60bf0e9a74a0
Your Name <kamaljeetkaur3k@gmail.com>
2019-02-02 00:00:53 -0800

    update

--- admin/include/links.php
+++ admin/include/links.php
@@ -7,6 +7,20 @@ if(isset($_POST['logout']))
 }
 
 
+
+
+
+if(isset($_POST['prayer']))
+{
+$name=$_POST['name'];
+$dob=$_POST['dob'];
+$address=$_POST['address'];
+$fname=$_POST['fname'];
+$contact=$_POST['contact'];
+$request=$_POST['request'];
+updatedata("prayer","name,dob,address,fname,contact,request","'$name','$dob','$address','$fname','$contact','$request'","index.php?pg=updatePrayer&id=$_GET['id'];");
+}
+
 ?>
 
 
